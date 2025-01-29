a775b6c24e55f776a4143163ffad156e5ea05834
Hatari Labs <hatariresearchlabs@gmail.com>
2025-01-29 15:14:19 -0500

    a
    a

--- tests/createVoronoiTests/testEx4TrenchExcavation.py
+++ tests/createVoronoiTests/testEx4TrenchExcavation.py
@@ -20,3 +20,29 @@ vorMesh.generateVoronoi()
 
 #Export generated voronoi mesh
 vorMesh.getVoronoiAsShp(outputPath='output')
+
+#check mesh generation
+from mf6Voronoi.meshProperties import meshShape
+import os
+
+# open the mesh file
+mesh=meshShape('output/'+vorMesh.modelDis['meshName']+'.shp')
+
+# get the list of vertices and cell2d data
+gridprops=mesh.get_gridprops_disv()
+
+cell2d = gridprops['cell2d']           #cellid, cell centroid xy, vertex number and vertex id list
+vertices = gridprops['vertices']       #vertex id and xy coordinates
+ncpl = gridprops['ncpl']               #number of cells per layer
+nvert = gridprops['nvert']             #number of verts
+centroids=gridprops['centroids']
+
+#check or create an output folder
+jsonPath = ('output/'+vorMesh.modelDis['meshName'])
+if os.path.isdir(jsonPath):
+    print('The output folder %s exists'%jsonPath)
+else:
+    os.mkdir(jsonPath)
+    print('The output folder %s has been generated.'%jsonPath)
+    
+mesh.save_properties(os.path.join(jsonPath,'disvDict.json'))
